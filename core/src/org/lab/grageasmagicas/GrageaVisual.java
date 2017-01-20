be76c691d1d815b0fc41e61ffc05f13d9b4eb04e
Ines <ines.kurchan@gmail.com>
2017-01-20 02:42:08 -0300

    Nada importante, cambie las imagenes de los botones nomas

--- core/src/org/lab/grageasmagicas/GrageaVisual.java
+++ core/src/org/lab/grageasmagicas/GrageaVisual.java
@@ -48,28 +48,28 @@ public class GrageaVisual {
 
     public void cargarImageButtonStyle() {
 
-        Texture gragea0Texture = new Texture(Gdx.files.internal("banana.png"));
-        Texture gragea1Texture = new Texture(Gdx.files.internal("blueBerry.png"));
-        Texture gragea2Texture = new Texture(Gdx.files.internal("cinnamon.png"));
-        Texture gragea3Texture = new Texture(Gdx.files.internal("lemon.png"));
-        Texture gragea4Texture = new Texture(Gdx.files.internal("greenApple.png"));
+        Texture gragea0Texture = new Texture(Gdx.files.internal("gragea0.png"));
+        Texture gragea1Texture = new Texture(Gdx.files.internal("gragea1.png"));
+        Texture gragea2Texture = new Texture(Gdx.files.internal("gragea2.png"));
+        Texture gragea3Texture = new Texture(Gdx.files.internal("gragea3.png"));
+        Texture gragea4Texture = new Texture(Gdx.files.internal("gragea4.png"));
 
-        Texture gragea0TextureSeleccionada = new Texture(Gdx.files.internal("banana_seleccionada.png"));
-        Texture gragea1TextureSeleccionada = new Texture(Gdx.files.internal("blueBerry_seleccionada.png"));
-        Texture gragea2TextureSeleccionada = new Texture(Gdx.files.internal("cinnamon_seleccionada.png"));
-        Texture gragea3TextureSeleccionada = new Texture(Gdx.files.internal("lemon_seleccionada.png"));
-        Texture gragea4TextureSeleccionada = new Texture(Gdx.files.internal("greenApple_seleccionada.png"));
+        Texture gragea0TextureSeleccionada = new Texture(Gdx.files.internal("gragea0seleccionada.png"));
+        Texture gragea1TextureSeleccionada = new Texture(Gdx.files.internal("gragea1seleccionada.png"));
+        Texture gragea2TextureSeleccionada = new Texture(Gdx.files.internal("gragea2seleccionada.png"));
+        Texture gragea3TextureSeleccionada = new Texture(Gdx.files.internal("gragea3seleccionada.png"));
+        Texture gragea4TextureSeleccionada = new Texture(Gdx.files.internal("gragea4seleccionada.png"));
 
-        Texture gragea0TextureSeleccionadaDoble = new Texture(Gdx.files.internal("banana_seleccionadaDoble.png"));
-        Texture gragea1TextureSeleccionadaDoble = new Texture(Gdx.files.internal("blueBerry_seleccionadaDoble.png"));
-        Texture gragea2TextureSeleccionadaDoble = new Texture(Gdx.files.internal("cinnamon_seleccionadaDoble.png"));
-        Texture gragea3TextureSeleccionadaDoble = new Texture(Gdx.files.internal("lemon_seleccionadaDoble.png"));
-        Texture gragea4TextureSeleccionadaDoble = new Texture(Gdx.files.internal("greenApple_seleccionadaDoble.png"));
+        Texture gragea0TextureSeleccionadaDoble = new Texture(Gdx.files.internal("gragea0seleccionadaDoble.png"));
+        Texture gragea1TextureSeleccionadaDoble = new Texture(Gdx.files.internal("gragea1seleccionadaDoble.png"));
+        Texture gragea2TextureSeleccionadaDoble = new Texture(Gdx.files.internal("gragea2seleccionadaDoble.png"));
+        Texture gragea3TextureSeleccionadaDoble = new Texture(Gdx.files.internal("gragea3seleccionadaDoble.png"));
+        Texture gragea4TextureSeleccionadaDoble = new Texture(Gdx.files.internal("gragea4seleccionadaDoble.png"));
 
         Button.ButtonStyle gragea0ButtonStyle = new Button.ButtonStyle();
         gragea0ButtonStyle.up = new TextureRegionDrawable(new TextureRegion(gragea0Texture));
-        gragea0ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea0TextureSeleccionada));
-        gragea0ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea0TextureSeleccionadaDoble));
+        gragea0ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea0TextureSeleccionadaDoble));
+        gragea0ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea0TextureSeleccionada));
 
         gragea0Style = new ImageButton.ImageButtonStyle(gragea0ButtonStyle);
         gragea0Style.imageDisabled = new TextureRegionDrawable(new TextureRegion(gragea0Texture));
@@ -77,24 +77,24 @@ public class GrageaVisual {
 
         Button.ButtonStyle gragea1ButtonStyle = new Button.ButtonStyle();
         gragea1ButtonStyle.up = new TextureRegionDrawable(new TextureRegion(gragea1Texture));
-        gragea1ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea1TextureSeleccionada));
-        gragea1ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea1TextureSeleccionadaDoble));
+        gragea1ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea1TextureSeleccionadaDoble));
+        gragea1ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea1TextureSeleccionada));
 
         gragea1Style = new ImageButton.ImageButtonStyle(gragea1ButtonStyle);
         gragea1Style.imageDisabled = new TextureRegionDrawable(new TextureRegion(gragea1Texture));
 
         Button.ButtonStyle gragea2ButtonStyle = new Button.ButtonStyle();
         gragea2ButtonStyle.up = new TextureRegionDrawable(new TextureRegion(gragea2Texture));
-        gragea2ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea2TextureSeleccionada));
-        gragea2ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea2TextureSeleccionadaDoble));
+        gragea2ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea2TextureSeleccionadaDoble));
+        gragea2ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea2TextureSeleccionada));
 
         gragea2Style = new ImageButton.ImageButtonStyle(gragea2ButtonStyle);
         gragea2Style.imageDisabled = new TextureRegionDrawable(new TextureRegion(gragea2Texture));
 
         Button.ButtonStyle gragea3ButtonStyle = new Button.ButtonStyle();
         gragea3ButtonStyle.up = new TextureRegionDrawable(new TextureRegion(gragea3Texture));
-        gragea3ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea3TextureSeleccionada));
-        gragea3ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea3TextureSeleccionadaDoble));
+        gragea3ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea3TextureSeleccionadaDoble));
+        gragea3ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea3TextureSeleccionada));
 
         gragea3Style = new ImageButton.ImageButtonStyle(gragea1ButtonStyle);
         gragea3Style.imageDisabled = new TextureRegionDrawable(new TextureRegion(gragea3Texture));
@@ -102,8 +102,8 @@ public class GrageaVisual {
 
         Button.ButtonStyle gragea4ButtonStyle = new Button.ButtonStyle();
         gragea4ButtonStyle.up = new TextureRegionDrawable(new TextureRegion(gragea4Texture));
-        gragea4ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea4TextureSeleccionada));
-        gragea4ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea4TextureSeleccionadaDoble));
+        gragea4ButtonStyle.down = new TextureRegionDrawable(new TextureRegion(gragea4TextureSeleccionadaDoble));
+        gragea4ButtonStyle.checked = new TextureRegionDrawable(new TextureRegion(gragea4TextureSeleccionada));
 
         gragea4Style = new ImageButton.ImageButtonStyle(gragea4ButtonStyle);
         gragea4Style.imageDisabled = new TextureRegionDrawable(new TextureRegion(gragea4Texture));
